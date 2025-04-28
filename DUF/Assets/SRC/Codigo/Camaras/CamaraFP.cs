f661c9ba3314f08563b340392e941f9bbc46de87
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-27 20:48:14 -0600

    Cámara primera persona funcionando

---
+++ DUF/Assets/SRC/Codigo/Camaras/CamaraFP.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFP : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float rotacionX;
+    float rotacionY;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        rotacionY += mouseX;
+
+        rotacionX -= mouseY;
+        rotacionX = Mathf.Clamp(rotacionX, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(rotacionX, rotacionY, 0);
+        orientation.rotation = Quaternion.Euler(0, rotacionY, 0);
+    }
+}
