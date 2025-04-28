0f391655702b9bb5ca00ed93988f95ca272399ad
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-04-27 21:10:02 -0600

    Movimiento fallido, avance para movimiento básico

---
+++ DUF/Assets/SRC/Codigo/Movimiento/MovimientoJugador.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovimientoJugador : MonoBehaviour
+{
+
+    [Header("Movimiento")]
+    public float velocidadMove;
+
+    public Transform orientacion;
+
+    float inputHorizontal;
+    float inputVertical;
+
+    Vector3 direccionMovimiento;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Inputs();
+    }
+
+    void FixedUpdate()
+    {
+        Movimiento();
+    }
+
+    private void Inputs() {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void Movimiento() {
+
+        direccionMovimiento = orientacion.forward * inputVertical + orientacion.right * inputHorizontal;
+
+        rb.AddForce(direccionMovimiento.normalized * velocidadMove * 10f, ForceMode.Force);
+    }
+}
