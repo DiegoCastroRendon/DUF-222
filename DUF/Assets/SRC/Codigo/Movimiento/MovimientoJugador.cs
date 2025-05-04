069936d416931e66832feaa404aa526df2acfb1f
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2025-05-03 22:49:09 -0600

    Correcciones del jugador, movimiento básico implementado

--- DUF/Assets/SRC/Codigo/Movimiento/MovimientoJugador.cs
+++ DUF/Assets/SRC/Codigo/Movimiento/MovimientoJugador.cs
@@ -9,6 +9,13 @@ public class MovimientoJugador : MonoBehaviour
     [Header("Movimiento")]
     public float velocidadMove;
     public float friccionPiso;
+    public float fuarzaSalto;
+    public float coolDownSalto;
+    public float multiplicadorAire;
+    bool puedeSaltar;
+
+    [Header("Controles")]
+    public KeyCode teclaSalto = KeyCode.Space;
 
     [Header("Check piso")]
     public float alturaJugador;
@@ -29,41 +36,70 @@ public class MovimientoJugador : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        rb.useGravity = true;
+        rb.mass = 1f;
+
+        puedeSaltar = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        tocandoPiso = Physics.Raycast(transform.position, Vector3.down, alturaJugador * (0.5f + 0.2f), piso );
-        
+
+        tocandoPiso = Physics.Raycast(transform.position, Vector3.down, alturaJugador * 0.5f + 0.2f, piso);
+
+        Debug.DrawRay(transform.position, Vector3.down * (alturaJugador * 0.5f + 0.2f), Color.red);
+
         Inputs();
 
         if(tocandoPiso) {
             rb.drag = friccionPiso;
-        } else {
-            rb.drag = 0f;
         }
 
-
+        Debug.Log("TocandoPiso: " + tocandoPiso);
+        
     }
 
     void FixedUpdate()
     {
         Movimiento();
         ControlVelocidad();
+        rb.AddForce(Vector3.down * 9.81f * rb.mass, ForceMode.Force);
+
     }
 
     private void Inputs() {
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         inputVertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(teclaSalto))
+        {
+            Debug.Log("Tecla de salto presionada");
+        }
+
+        if (Input.GetKey(teclaSalto) && puedeSaltar && tocandoPiso) {
+            puedeSaltar = false;
+            Salto();
+            Invoke(nameof(ResetSalto), coolDownSalto);
+        }
     }
 
     private void Movimiento() {
 
         direccionMovimiento = (orientacion.forward * inputVertical) + (orientacion.right * inputHorizontal);
+
+        
         //direccionMovimiento.y = 0f;
 
-        rb.AddForce(10f * velocidadMove * direccionMovimiento.normalized, ForceMode.Force);
+
+        if(tocandoPiso)
+            rb.AddForce(10f * velocidadMove * direccionMovimiento.normalized, ForceMode.Force);
+
+        else if (!tocandoPiso)
+            rb.AddForce(10f * multiplicadorAire * velocidadMove * direccionMovimiento.normalized, ForceMode.Force);
+
+
+
     }
 
     private void ControlVelocidad() {
@@ -73,5 +109,15 @@ public class MovimientoJugador : MonoBehaviour
             Vector3 velLimite = velPlana.normalized * velocidadMove;
             rb.velocity = new Vector3(velLimite.x, rb.velocity.y, velLimite.z);
         }
+    }
+
+    private void Salto() {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * fuarzaSalto, ForceMode.Impulse);
+    }
+
+    private void ResetSalto() {
+        puedeSaltar = true;
     }
 }
